b864ea42c1dff56a4b9b3cf13146b8c815119079
aleksvin8888 <aleksvin8888@gmail.com>
2024-09-14 18:43:42 +0300

    add logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application", slog.Any("sfg", cfg))
 
-	fmt.Println(cfg)
-	// TODO ініціювати логер
 	// TODO ініціювати додаток ( app )
 	// TODO запуск gRPC-сервера
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
