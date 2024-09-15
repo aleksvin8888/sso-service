bad224af5642eeeb98cc35a84d8b7b3d5c0b95f2
aleksvin8888 <aleksvin8888@gmail.com>
2024-09-15 10:50:47 +0300

    implement gRPC server

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log/slog"
 	"os"
+	"os/signal"
+	"sso/internal/app"
 	"sso/internal/config"
 	"sso/internal/lib/logger/slogpretty"
+	"syscall"
 )
 
 const (
@@ -18,8 +21,21 @@ func main() {
 	log := setupLogger(cfg.Env)
 	log.Info("Starting application", slog.Any("sfg", cfg))
 
-	// TODO ініціювати додаток ( app )
-	// TODO запуск gRPC-сервера
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCSrv.MustRun()
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("application stopped")
 
 }
 
---
+++ internal/app/grpc/app.go
@@ -0,0 +1,63 @@
+package grpcapp
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log/slog"
+	"net"
+	authgrpc "sso/internal/grps/auth"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+// New create new gRPC server app
+func New(log *slog.Logger, port int) *App {
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+// MustRun runs gRPC server and panic if get eny errors
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+// Run start gRPC server
+func (a *App) Run() error {
+	const op = "grpcapp.run"
+	log := a.log.With(slog.String("op", op), slog.Int("port", a.port))
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("gRPC server is running ", slog.String("addr ", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+// Stop Graceful stop gRPS server
+func (a *App) Stop() {
+	const op = "grpcapp.stop"
+
+	a.log.With(slog.String("op", op)).Info("Stopping gRPC server ", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
+}
